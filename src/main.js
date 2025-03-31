d7fe04d0a4a0596b7faad9ee12a3ee21b053beb5
imclaremont <dhha1515@gmail.com>
2025-03-31 14:34:50 +0900

    부트스트랩 dependency 추가

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/font/bootstrap-icons.css'
+
 createApp(App).mount('#app')
