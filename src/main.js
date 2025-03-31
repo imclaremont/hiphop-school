0064fa4d11dab360fb1820052ff509d74e050942
하동헌 <149583958+imclaremont@users.noreply.github.com>
2025-03-31 15:12:27 +0900

    [fix] 부트스트랩 아이콘 폰트 경로 수정

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
-import 'bootstrap/dist/font/bootstrap-icons.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App).mount('#app')
